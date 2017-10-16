377d7bbfa29886cfcc5ac8be3a67f10083c519a6
Dunk <21duncanlo@gmail.com>
2017-10-16 15:26:52 -0700

    Updates all resources. It may be faulty.

---
+++ MageTower/Assets/FinanceDisplayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinanceDisplayer : MonoBehaviour {
+    /* ROLE:
+     * Updates the financial information display on the top left corner of the canvas.
+     */
+
+    //CLASS VARIABLES
+
+    //COMPONENTS
+    Text txt;
+    
+    //IMPORTANT GAME OBJECTS
+    GameController gc;
+    
+    //CLASS FUNCTIONS
+
+	void Start () {
+        //DEFINE COMPONENTS
+        txt = GetComponent<Text>();
+        
+        gc = GameController.main;
+        //define the GameController (to obtain finacial information)
+	}
+	
+	void Update () {
+        txt.text = "Anima: " + gc.finance;
+	}
+}
